1f091523fb42e6652de2ae11675a405f300797b2
Valentinos Pariza <valentinospariza@gmail.com>
2023-09-07 20:08:38 +0200

    Website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import UsersJson from './users.json';
+  
+interface USERS {
+    id: Number;
+    name: String;
+    username: String;
+    email: String;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ir-dashboard';
+  
+  Users: USERS[] = UsersJson;
+  constructor(){
+    console.log(this.Users);
+  }
 }
